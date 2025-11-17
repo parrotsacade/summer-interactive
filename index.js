db671cb839fe2f135145b1eb16e9aef33a06c5b7
tawsifalmahmud99-sudo <tawssifalmahmud99@gmail.com>
2025-11-17 14:47:26 +0600

    form active

--- index.js
+++ index.js
@@ -19,13 +19,44 @@
 
 //THE BEST WAY OF PRACTISE
 
-  let totalprice=0
+let totalprice = 0;
+let discountPrice = 0;
 
-function getUniversalData(card){
-    const name=card.dataset.name
-   const price=parseInt(card.dataset.price)
- 
-   totalprice+=price
-    console.log(name,price)
-    console.log("Totalprice",totalprice)
+const displayElement = document.getElementById("displayName");
+
+const totalPriceElement = document.getElementById("totalPrice");
+const discountElement = document.getElementById("discount");
+const totalElement = document.getElementById("total");
+
+
+function getUniversalData(card) {
+  const name = card.dataset.name;
+  const price = parseInt(card.dataset.price);
+
+  totalprice += price;
+  console.log(name, price);
+  console.log("Totalprice", totalprice);
+
+  let initialDiscountTotal = totalprice;
+
+  const li = document.createElement("li");
+  li.classList.add("list-decimal");
+  li.innerText = name;
+  displayElement.appendChild(li);
+
+  totalPriceElement.innerText = totalprice;
+
+  totalElement.innerText = initialDiscountTotal;
+}
+
+
+function discountCal() {
+  const inputDiscount = document.getElementById("discountValue").value;
+   console.log(inputDiscount);
+   console.log(totalprice);
+   discountPrice = parseInt(totalprice * (inputDiscount/100))
+   discountElement.innerText= discountPrice
+   console.log(discountPrice,"discountPrice");
+   totalElement.innerText = totalprice-discountPrice;
+   
 }
